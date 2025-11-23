dea5ab1bee5a84522f37c6b9b5e71ca5b7c1a15d
Felipe <felipesorrentino89@gmail.com>
2025-11-23 15:02:20 -0300

    feat: add product routes

--- src/model/product.py
+++ src/model/product.py
@@ -4,4 +4,11 @@ class Product:
         self.name = name
         self.price = price
         self.id_category = id_category
-    
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'id_category': self.id_category
+        }
